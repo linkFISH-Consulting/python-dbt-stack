81d2ddbdf29f5785463e526596f010065cdf4c03
pSpitzner <github@makeitso.one>
2026-04-10 15:59:57 +0200

    Added component to print dbt versions

--- src/lf_py_stack/orchestration/__init__.py
+++ src/lf_py_stack/orchestration/__init__.py
@@ -1,5 +1,5 @@
 from .cli import cli_app
-from .components import run_cli_command, truncate
+from .components import get_dbt_versions, log_dbt_versions, run_cli_command, truncate
 from .logger import get_logger
 from .mail import mail_app, send_mail
 from .steps import StepResult, log_step_nodes_table, log_step_results_table
@@ -8,6 +8,8 @@ __all__ = [
     "StepResult",
     "log_step_nodes_table",
     "log_step_results_table",
+    "log_dbt_versions",
+    "get_dbt_versions",
     "run_cli_command",
     "get_logger",
     "send_mail",
